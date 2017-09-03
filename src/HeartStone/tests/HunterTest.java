b944d2e154ef18f3a332d600d29f951f46df8c5c
DanielaCampos <daniela.campos.f@ing.uchile.cl>
2017-09-03 08:22:04 -0300

    <Codigo Fuente, Test, JavaCode y CodingStyle  Listo>

--- src/HeartStone/tests/HunterTest.java
+++ src/HeartStone/tests/HunterTest.java
@@ -6,54 +6,54 @@ import org.junit.*;
 import HeartStone.*;
 
 public class HunterTest {
-	private Assassin a;
-	private Druid d;
-	private Hunter hu;
-	private Mage m;
-	
-	@Before
-	public void setUp() throws Exception {
-		a = new Assassin("Juan", 100, 10);
-		d = new Druid("Jose", 90, 8);
-		hu = new Hunter("Felipe", 80, 14);
-		m = new Mage("Francisco", 100, 8);
-		
-	}
-	
-	@Test
-	public void testPlayerHasName() {
+  private Assassin a;
+  private Druid d;
+  private Hunter hu;
+  private Mage m;
 
-		assertNotNull(hu.getName());
-		assertEquals(hu.getName(), "Felipe");
-	}
-	
-	@Test
-	public void testPoints() {
-		
-		assertNotNull(hu.getLifePoints());
-		assertEquals(hu.getLifePoints(), 80);
-		
-		assertNotNull(hu.getActionPoints());
-		assertEquals(hu.getActionPoints(), 14);
-		
-		assertNotNull(hu.getDamagePoints());
-		assertEquals(hu.getDamagePoints(), 0);
-	}
-	
-	
-	@Test
-	public void testInteraction() {
-		
-		hu.attack(a);
-		assertEquals(a.getDamagePoints(), 14);
-		
-		hu.receivesAttackDruid(d);
-		assertEquals(hu.getDamagePoints(), 4);
-		
-		hu.receivesAttackMage(m);
-		assertEquals(hu.getDamagePoints(), 20);
-		
-	}
-	
+  @Before
+  public void setUp() throws Exception {
+    a = new Assassin("Juan", 100, 10);
+    d = new Druid("Jose", 90, 8);
+    hu = new Hunter("Felipe", 80, 14);
+    m = new Mage("Francisco", 100, 8);
+
+  }
+
+  @Test
+  public void testPlayerHasName() {
+
+    assertNotNull(hu.getName());
+    assertEquals(hu.getName(), "Felipe");
+  }
+
+  @Test
+  public void testPoints() {
+
+    assertNotNull(hu.getLifePoints());
+    assertEquals(hu.getLifePoints(), 80);
+
+    assertNotNull(hu.getActionPoints());
+    assertEquals(hu.getActionPoints(), 14);
+
+    assertNotNull(hu.getDamagePoints());
+    assertEquals(hu.getDamagePoints(), 0);
+  }
+
+
+  @Test
+  public void testInteraction() {
+
+    hu.attack(a);
+    assertEquals(a.getDamagePoints(), 14);
+
+    hu.receivesAttackDruid(d);
+    assertEquals(hu.getDamagePoints(), 4);
+
+    hu.receivesAttackMage(m);
+    assertEquals(hu.getDamagePoints(), 20);
+
+  }
+
 
 }
--- src/HeartStone/tests/PaladinTest.java
+++ src/HeartStone/tests/PaladinTest.java
@@ -6,55 +6,55 @@ import org.junit.*;
 import HeartStone.*;
 
 public class PaladinTest {
-	private Assassin a;
-	private Knight k;
-	private Mage m;
-	private Paladin p;
+  private Assassin a;
+  private Knight k;
+  private Mage m;
+  private Paladin p;
 
-	
-	@Before
-	public void setUp() throws Exception {
-		
-		a = new Assassin("Juan", 100, 10);
-		k = new Knight("Joaquin", 70, 16);
-		m = new Mage("Francisco", 100, 8);
-		p = new Paladin("Pedro", 90, 6);
-	}
-	
-	@Test
-	public void testPlayerHasName() {
 
-		assertNotNull(p.getName());
-		assertEquals(p.getName(), "Pedro");
-	}
-	
-	@Test
-	public void testPoints() {
-		
-		assertNotNull(p.getLifePoints());
-		assertEquals(p.getLifePoints(), 90);
-		
-		assertNotNull(p.getActionPoints());
-		assertEquals(p.getActionPoints(), 6);
-		
-		assertNotNull(p.getDamagePoints());
-		assertEquals(p.getDamagePoints(), 0);
-	}
-	
-	
-	@Test
-	public void testInteraction() {
-		
-		p.attack(a);
-		assertEquals(a.getDamagePoints(), 0);
-		assertEquals(a.getActionPoints(), 12);
-		
-		p.receivesAttackKnight(k);
-		assertEquals(p.getDamagePoints(), 8);
-		
-		p.receivesAttackMage(m);
-		assertEquals(p.getDamagePoints(), 24);	
-	}
-	
+  @Before
+  public void setUp() throws Exception {
+
+    a = new Assassin("Juan", 100, 10);
+    k = new Knight("Joaquin", 70, 16);
+    m = new Mage("Francisco", 100, 8);
+    p = new Paladin("Pedro", 90, 6);
+  }
+
+  @Test
+  public void testPlayerHasName() {
+
+    assertNotNull(p.getName());
+    assertEquals(p.getName(), "Pedro");
+  }
+
+  @Test
+  public void testPoints() {
+
+    assertNotNull(p.getLifePoints());
+    assertEquals(p.getLifePoints(), 90);
+
+    assertNotNull(p.getActionPoints());
+    assertEquals(p.getActionPoints(), 6);
+
+    assertNotNull(p.getDamagePoints());
+    assertEquals(p.getDamagePoints(), 0);
+  }
+
+
+  @Test
+  public void testInteraction() {
+
+    p.attack(a);
+    assertEquals(a.getDamagePoints(), 0);
+    assertEquals(a.getActionPoints(), 12);
+
+    p.receivesAttackKnight(k);
+    assertEquals(p.getDamagePoints(), 8);
+
+    p.receivesAttackMage(m);
+    assertEquals(p.getDamagePoints(), 24);
+  }
+
 
 }
